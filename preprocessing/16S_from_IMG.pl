ee447c061f9942d56c86e015671e261020b02ad5
Florent Angly <florent.angly@gmail.com>
2012-10-19 15:58:21 +1000

    Misc changes

--- preprocessing/16S_from_IMG.pl
+++ preprocessing/16S_from_IMG.pl
@@ -1,10 +1,28 @@
 #!/usr/bin/env perl
+
+
+=head1 NAME
+
+16S_from_IMG.pl
+ 
+=head1 DESCRIPTION
+
+Gets 16S sequences from folders of IMG taxon ids (with gffs and gene fasta files)
+    
+=head1 SYNOPSIS
+
+  16S_from_IMG.pl -i <folder_containing_taxon_subfolders>
+        -i      Input directory
+        
+=cut
+
 use strict;
 use warnings;
 
 use Getopt::Long;
-use Data::Dumper;
 use Bio::SeqIO;
+use File::Spec;
+
 
 my $options = check_params();
 
@@ -16,36 +34,38 @@ my %genes;
 
 my $out_fh = Bio::SeqIO->new(-format => 'fasta');
 
-opendir(my $dh, $options->{'i'});
-while(my $entry = readdir($dh)) {
-    if (-e $options->{'i'} . "/$entry/$entry.gff") {
-        open(my $fh, "<", $options->{'i'} . "/$entry/$entry.gff");
-        while (my $line = <$fh>) {
-            chomp $line;
-            my @splitline = split /\t/, $line;
-            if (scalar(@splitline) < 9) {
-                next;
+my $input_dir = $options->{'i'};
+opendir my $dh, $input_dir or die "Error: Could not read folder $input_dir\n$!\n";
+while (my $entry = readdir($dh)) {
+   my $gff_file = File::Spec->catfile($input_dir, $entry, "$entry.gff");
+   if (-e $gff_file) {
+      open my $fh, '<', $gff_file or die "Error: Could not read file $gff_file\n$!\n";
+      while (my $line = <$fh>) {
+         chomp $line;
+         my @splitline = split /\t/, $line;
+         if (scalar(@splitline) < 9) {
+            next;
+         }
+         if ($splitline[2] eq 'rRNA') {
+            if ($splitline[8] =~ /16[Ss]/ && $splitline[8] =~ /ID=(\d+)/) {
+               $genes{$1} = $entry;
             }
-            if ($splitline[2] eq 'rRNA') {
-                if ($splitline[8] =~ /16[Ss]/ && $splitline[8] =~ /ID=(\d+)/) {
-                    $genes{$1} = $entry;   
-                }
-            }
-        }
-        if (-e $options->{'i'} . "/$entry/$entry.genes.fna") {
-            my $in_fh = Bio::SeqIO->new(-format => 'fasta',
-                                        -file => $options->{'i'} .
+         }
+      }
+      if (-e $options->{'i'} . "/$entry/$entry.genes.fna") {
+         my $in_fh = Bio::SeqIO->new(-format => 'fasta',
+                                     -file => $options->{'i'} .
                                                  "/$entry/$entry.genes.fna");
-            while (my $seq_obj = $in_fh->next_seq()) {
-                if (exists($genes{$seq_obj->id()})) {
-                    $seq_obj->id($genes{$seq_obj->id()} . "_" . $seq_obj->id());
-                    $out_fh->write_seq($seq_obj)
-                }
+         while (my $seq_obj = $in_fh->next_seq()) {
+            if (exists($genes{$seq_obj->id()})) {
+               $seq_obj->id($genes{$seq_obj->id()} . "_" . $seq_obj->id());
+               $out_fh->write_seq($seq_obj)
             }
-        }
-    }
+         }
+      }
+   }
 }
-closedir($dh);
+closedir $dh;
 
 ################################################################################
 # Subroutine: check_params()
@@ -63,21 +83,3 @@ sub check_params {
     return \%options;
 }
 
-__DATA__
-
-=head1 NAME
-
-	16S_from_IMG.pl  
- 
-=head1 DESCRIPTION
-
-    Gets 16S sequences from folders of IMG taxon ids (with gffs and gene fasta files)
-    
-=head1 SYNOPSIS
-
-    16S_from_IMG.pl -i <folder_containing_taxon_subfolders>
-
-        -i      Input directory
-
-        
-=cut
