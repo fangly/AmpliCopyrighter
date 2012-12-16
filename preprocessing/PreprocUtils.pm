24e49ad5153af5686316d829ce69662423cc43b0
Florent Angly <florent.angly@gmail.com>
2012-12-17 00:10:15 +1000

    Delegated some functions to the dedicated PreprocUtils module

---
+++ preprocessing/PreprocUtils.pm
@@ -0,0 +1,55 @@
+package PreprocUtils;
+
+use strict;
+use warnings;
+use Method::Signatures;
+
+
+func find_column_for ($header_line, @field_names) {
+   # Given a tab-separated header line, find which column matches the given name.
+   # Search is insensitive to case, spaces, underscores and dashes. Also, note
+   # that matches only need to be partial.
+   my @fields = split /\t/, $header_line;
+   # Make names and cols insensitive
+   for my $arr ( \@fields, \@field_names ) {
+      for my $i (0 .. scalar @$arr - 1) {
+         $arr->[$i] =~ s/[\s_-]//g;
+         $arr->[$i] = lc $arr->[$i];
+      }
+   }
+   # Look for names in column headers
+   my @col_nums;
+   for my $name (@field_names) {
+      my $col_num;
+      for ($col_num = 0; $col_num < scalar @fields; $col_num++) {
+         my $field = $fields[$col_num];
+         if ($field =~ m/$name/) {
+            push @col_nums, $col_num;
+            last;
+         }
+      }
+      if (not defined $col_num) {
+         die "Error: Could not find column holding '$name' data\n";
+      }
+   }
+   return @col_nums;
+}
+
+
+func read_lookup ($file) {
+   # Read a 2-column, tab-delimited lookup file (e.g. Greengenes taxonomy,
+   # img_to_gg, etc)
+   my $lookup;
+   open my $fh, '<', $file or die "Error: Could not read file $file\n$!\n";
+   while (my $line = <$fh>) {
+      chomp $line;
+      next if $line =~ m/^#/;
+      my ($key, $val) = split /\t/, $line;
+      $lookup->{$key} = $val;
+   }
+   close $fh;
+   return $lookup;
+}
+
+
+1;
